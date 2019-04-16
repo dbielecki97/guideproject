854bfbe89c3692aec6f60f1e7d1ea61aec295c02
Dawid Bielecki <dbielecki97@gmail.com>
2019-04-16 12:47:50 +0200

    wszystko z zajęć

--- guide/urls.py
+++ guide/urls.py
@@ -3,4 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('attractions', views.AttractionListView.as_view(), name='attractions'),
+    path('attractions/<int:pk>',
+         views.AttractionDetailView.as_view(), name='attraction-detail'),
+    path('shoppingcard/<int:pk>',
+         views.ShoppingCartDetailView.as_view(), name='shopping-cart'),
+    path('mytripplans/', views.TripPlanListView.as_view(), name='my-trip-plans'),
+    path('mytripplans/<int:pk>', views.TripPlanDetailView.as_view(),
+         name='trip-plan-detail'),
 ]
--- guide/views.py
+++ guide/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
-
+from django.views.generic import DetailView, ListView
+from .models import Attraction, TripPlan, Localization, Category, Client, ShoppingCart
 # Create your views here.
 
 
 def home(request):
     return render(request, 'home.html')
+
+
+class AttractionListView(ListView):
+    model = Attraction
+
+
+class AttractionDetailView(DetailView):
+    model = Attraction
+
+
+class TripPlanListView(ListView):
+    model = TripPlan
+
+
+class TripPlanDetailView(DetailView):
+    model = TripPlan
+
+
+class ShoppingCartDetailView(DetailView):
+    model = ShoppingCart
