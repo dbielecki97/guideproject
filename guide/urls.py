f7aaf57018e8d6d98bd1d6a1d858022aee99d513
Dawid Bielecki <dbielecki97@gmail.com>
2019-04-16 10:55:55 +0200

    removed migrations folder

--- guide/urls.py
+++ guide/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('/shopping-cart', views.shoppingCart, name='shopping-cart'),
+    path('', views.home, name='home'),
 ]
