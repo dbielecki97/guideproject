a659c449c76dce1594b72b38a1322cd748cebb78
mysloo <supergracz20@interia.pl>
2019-05-09 21:19:08 +0200

    Poprawiona wersja

--- guide/forms.py
+++ guide/forms.py
@@ -1,23 +1,25 @@
 from django import forms
-from django.contrib.auth.models import User
+from guide.models import Client
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth.models import User
 
 class SaveTripPlanForm(forms.Form):
     name = forms.CharField(label='Nazwa planu', max_length=150)
 
 
 class SignUpForm(UserCreationForm):
-    #email = forms.EmailField(required=True)
+    name = forms.CharField(label='Firstname',max_length=50)
+    surname = forms.CharField(label='Surname', max_length=50)
 
     class Meta:
-        model = User
-        fields = ("username", "password1", "password2")
+        model = Client
+        fields = ("username", "name","surname", "password1", "password2")
 
-""" @override save func from UserCreationForm
-    def save(self, commit=True):
-        user = super(UserCreateForm, self).save(commit=False)
-        user.email = self.cleaned_data["email"]
-        if commit:
-            user.save()
-        return user """
+        # @override save func from UserCreationForm
+        def save(self, commit=True):
+            user = super(UserCreationForm, self).save(commit=False)
+            user.name = self.cleaned_data["name"]
+            user.surname = self.cleaned_data["surname"]
+            if commit:
+                user.save()
+            return user 
