c318490192361b71823c29f2adb3a94174add95b
Dawid Bielecki <dbielecki97@gmail.com>
2019-05-22 13:28:33 +0200

    filtrowanie po kategorii w liscie atrakcji

--- guide/filters.py
+++ guide/filters.py
@@ -3,14 +3,6 @@ import django_filters
 from .models import Attraction, Category
 
 
-# class AttractionListFilter(django_filters.FilterSet):
-#     fields = django_filters.ModelChoiceField(label='')
-
-#     class Meta:
-#         model = Attraction
-#         fields = ['categories']
-
-
 class AttractionListFilter(django_filters.FilterSet):
     categories = django_filters.ModelChoiceFilter(
         label='Kategoria', queryset=Category.objects.all(), empty_label="wszystkie")
--- guideproject/settings.py
+++ guideproject/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'guide.apps.GuideConfig'
+    'guide.apps.GuideConfig',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
